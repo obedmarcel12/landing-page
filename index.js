4970838f86bad01123d78e2491c9441bf3d5d29d
obedgodfrey <obedmarcelcomedian@gmail.com>
2024-08-12 15:23:00 +0100

    commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//
 function toggleMenu() {
   const navItems = document.querySelector('.nav-items')
   const login = document.querySelector('.login')
@@ -6,3 +7,20 @@ function toggleMenu() {
   login.classList.toggle('mobile-active')
   removebg.classList.toggle('mobile-active')
 }
+
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  let message = []
+  const email = document.getElementById('email').value
+  if (email === '' || email == null) {
+    message.push('Input E-mail')
+  } else {
+    window.location.href = 'https://www.youtube.com/account'
+  }
+
+  if (message.length > 0) {
+    const error = document.querySelector('.error')
+    error.innerText = message.join(' , ')
+  }
+})
